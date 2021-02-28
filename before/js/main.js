545c7346a757b67eb48d532e4745aaab03cf2445
fourwujuprb <75727461+fourwujuprb@users.noreply.github.com>
2021-02-28 10:38:08 +0100

    Add files via upload

---
+++ before/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    // console.log('jquery działa');
+    // console.log($('.accordion-item'));
+    $('.accordion-item').click(function () {
+        // console.log('klikam');
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $(this).find('.accordion-content').slideUp();
+        } else {
+            $(this).addClass('open');
+            $(this).find('.accordion-content').slideDown();
+        }
+    });
+});
